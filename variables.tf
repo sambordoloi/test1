8537c2dd5bd13300ef79c80a5dc5f47c867e35f3
sambordoloi <75413841+sambordoloi@users.noreply.github.com>
2021-09-04 13:04:31 +0530

    adding variables

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "accesskey" {
+  default = ""    #enter accesskey
+}
+variable "secretkey" {
+  default = ""    #enter secretkey
+}
+variable "subnetid" {
+  default = ""    #enter vpc subnet id
+}
+variable "sg" {
+  default = ""    #enter sg id
+}
+variable "sslcert" {
+  default = ""     #enter domain ssl cert arn id
+}
+variable "dbuser" {
+  default = ""     #enter user name
+}
+variable "dbpasswd" {
+  default = ""   #enter db passwd
+}
+
